65008b452a7a310d408d1f5037aff03072437277
Devin Jiang <dj6207@rit.edu>
2022-10-12 11:31:56 -0400

    training

--- autoSlime.py
+++ autoSlime.py
@@ -1,12 +1,13 @@
 # GBF Auto Slime
 import cv2 as cv
 from time import time
+import torch
 from Ai import WindowCapture
 
 # When screen shotting from chrome make sure to turn off hardware acceleration or else black screen
 # initialize the WindowCapture class
-wincap = WindowCapture('Granblue Fantasy - Google Chrome')
-# wincap = WindowCapture('Google Chrome')
+# wincap = WindowCapture('Granblue Fantasy - Google Chrome')
+wincap = WindowCapture('Task Manager')
 
 loop_time = time()
 while(True):
@@ -14,11 +15,16 @@ while(True):
     # get an updated image of the game
     screenshot = wincap.get_screenshot()
 
-    cv.imshow('Computer Vision', screenshot)
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    results = model(screenshot)
+    results.show()
+
+
+    # cv.imshow('Computer Vision', screenshot)
 
     # debug the loop rate
-    print('FPS {}'.format(1 / (time() - loop_time)))
-    loop_time = time()
+    # print('FPS {}'.format(1 / (time() - loop_time)))
+    # loop_time = time()
 
     # press 'q' with the output window focused to exit.
     # waits 1 ms every loop to process key presses
